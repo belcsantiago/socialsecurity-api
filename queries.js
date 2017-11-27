0ef3d75d5272aedc009a71fe45cb6b485a4a47ea
Isabel Santiago <32544865+belcsantiago@users.noreply.github.com>
2017-11-27 08:41:49 -0300

    Update queries.js

--- queries.js
+++ queries.js
@@ -65,10 +65,10 @@ function createUser(req, res, next) {
     });
 }
 
-/* Login queries functions
+/* Login queries functions */
 
 function verifyLogin(req, res, next) {
-  db.one('select * from usuarios where email_usuario = ${email_usuario} and senha_usuario = ${senha_usuario}',[req.params.email_usuario, req.params.senha_usuario])
+  db.one('select * from usuarios where email_usuario = $1 and senha_usuario = $2',[req.params.email_usuario, req.params.senha_usuario])
       .then(function (data) {
         res.status(200)
           .json({
@@ -81,7 +81,7 @@ function verifyLogin(req, res, next) {
         return next(err);
       });
 }
- */
+
 
 /*
 function updatePuppy(req, res, next) {
